2b1c599a6f25f3af67b106a77e4c559428b66f7e
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-11-17 01:10:19 +0530

    Update script.sh

--- PartA_backend/script.sh
+++ PartA_backend/script.sh
@@ -3,6 +3,8 @@
 gnome-terminal -- zsh -c "python3 ./Backend_endpoint/commsBack.py; exec zsh"
 sleep 2
 
+# waiting for the programs to start their sockets. Won't really be necessary after the first time, but works better this way.
+
 gnome-terminal -- zsh -c "python3 ./terminal_gui/termEMU.py; exec zsh"
 sleep 2
 
