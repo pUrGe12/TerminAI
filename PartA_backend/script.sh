898973a43ec6a2dd0e1a5cd44ad64d94fbbbf092
pUrGe12 <helloitsme0218@gmail.com>
2024-11-17 00:57:01 +0530

    created script for ease

---
+++ PartA_backend/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gnome-terminal -- zsh -c "python3 ./Backend_endpoint/commsBack.py; exec zsh"
+sleep 2
+
+gnome-terminal -- zsh -c "python3 ./terminal_gui/termEMU.py; exec zsh"
+sleep 2
+
+gnome-terminal -- zsh -c "python3 ./Sequencer/sequencer.py; exec zsh"
+sleep 2
+
+for i in {5001..5006}; do
+    gnome-terminal -- zsh -c "python3 ./extraction_models/client_${i}.py; exec zsh"
+done
