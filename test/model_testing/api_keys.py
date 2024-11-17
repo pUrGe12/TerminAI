2da7e7a016ba1efb7674f98ca41f01e9422adbe0
pUrGe12 <helloitsme0218@gmail.com>
2024-11-18 01:01:08 +0530

    removed api keys

--- test/model_testing/api_keys.py
+++ test/model_testing/api_keys.py
@@ -1,10 +1,10 @@
 # place your API key here
 apikey_dict = {
-	"gemini_key": "AIzaSyDgtJZg8o9fYUlJm9xeYNkRwzQ2nbZiHQI" 											
+	"gemini_key": "" 											
 }
 
 # place your Supabase keys here
 supabase_key_dict = {
-	"url": "https://yvcmpdgbeopnscegtjoh.supabase.co",			
-	"key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2Y21wZGdiZW9wbnNjZWd0am9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzNjAxMzUsImV4cCI6MjA0NTkzNjEzNX0.n2FKKYLLO_IhtWtjsZfNSVic5mnuXmu5dCA9mbk1SfU"
+	"url": "",			
+	"key": ""
 }
